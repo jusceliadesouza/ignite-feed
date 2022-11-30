30e6e72d7b1e491cbcc966f496c5312cc1405691
Juscélia de Souza <jusceliadesousa@gmail.com>
2022-11-30 19:11:13 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Post from "./Post";
+
+export default function App() {
+  return (
+    <div>
+      <Post
+        author="Jane Cooper"
+        content="Aute minim et amet minim velit ex cillum voluptate."
+      />
+
+      <Post
+        author="Devon Lane"
+        content="Consectetur magna mollit aliqua fugiat mollit velit elit enim minim eiusmod tempor."
+      />
+    </div>
+  );
+}
